70d193f40835f4a4858569ac9dcda258aea7ffdf
mac_softwarefactory_1 <mac_softwarefactory_1@172.16.1.111>
2016-07-12 16:32:25 +0700

    commit

--- src/main/java/lexto/Main.java
+++ src/main/java/lexto/Main.java
@@ -8,15 +8,13 @@ public class Main {
 	LongLexTo tokenizer;
 	public static void main(String[] args) {
 		String input = "test";
-		String text5 = "บริการไม่ดีเบื่อหน่ายห่วย";
+		String text5 = "ทำอะรัยอยู่ช้าล้าสมัย";
 		new Main().TokenzerTest( text5);
 		}
 	
 	private void TokenzerTest(String input){
 		try {
 			tokenizer = new LongLexTo();
-	
-		
 			System.out.println(tokenizer.parseToString(input));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
